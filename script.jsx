c73f937a8a6c63cf31c5cba2ef964823d32f5534
Yasmin Serag <159052828+yasminserag08@users.noreply.github.com>
2025-08-31 22:03:35 +0300

    Add chat message and chat input components

--- script.jsx
+++ script.jsx
@@ -1,4 +1,10 @@
 const container = document.querySelector('.js-container'); 
+let props = {
+  message: 'Hello chatbot',
+  sender: 'user'
+};
+const { message } = props;
+const { sender } = props;
 
 function ChatInput() {
   return (
@@ -9,9 +15,31 @@ function ChatInput() {
   );
 }
 
+function ChatMessage(props) {
+  const message = props.message;
+  const sender = props.sender;
+
+  if(sender === 'bot') {
+    return (
+      <div>
+        <img src="bot.png" width="50px" />
+        {message}
+      </div>
+    );
+  }
+  return (
+    <div>
+      {message}
+      <img src="user.png" width="50px" />
+    </div>
+  );
+} 
+
 const app = (
   <>
     <ChatInput />
+    <ChatMessage message='Hello chatbot' sender='user' />
+    <ChatMessage message='Hello user' sender='bot' />
   </>
 )
 

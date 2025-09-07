f680eaa147365524aae1d785ebbdbe66ca8a0fcb
Yasmin Serag <159052828+yasminserag08@users.noreply.github.com>
2025-09-07 18:40:19 +0300

    Add spacing

--- script.jsx
+++ script.jsx
@@ -86,14 +86,14 @@ function ChatMessage({ message, sender }) {
 function ChatMessages({ chatMessages }) 
 {
   return (
-    <>
+    <div className="chat-messages-container">
       {chatMessages.map((chatMessage) => {
         return <ChatMessage 
           message={chatMessage.message} 
           sender={chatMessage.sender} 
           key={chatMessage.id} />;
       })}
-    </>
+    </div>
   );
 }
 
